cc4a11ab42bc891c7697d753020378981ed6c0e8
laien <laienwu0112@gmail.com>
2026-02-01 22:06:26 +0100

    ✨ "Add transform"

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -8,5 +8,30 @@ Contains modules for:
 """
 
 from .ingest import fetch_klines, fetch_all_symbols, ingest_data, fetch_current_prices
+from .transform import (
+    transform_data,
+    load_processed_metrics,
+    calculate_log_returns,
+    calculate_volatility,
+    calculate_correlation_matrix,
+    calculate_covariance_matrix,
+    calculate_mean_returns,
+    TransformError,
+)
 
-__all__ = ["fetch_klines", "fetch_all_symbols", "ingest_data", "fetch_current_prices"]
+__all__ = [
+    # Ingest
+    "fetch_klines",
+    "fetch_all_symbols",
+    "ingest_data",
+    "fetch_current_prices",
+    # Transform
+    "transform_data",
+    "load_processed_metrics",
+    "calculate_log_returns",
+    "calculate_volatility",
+    "calculate_correlation_matrix",
+    "calculate_covariance_matrix",
+    "calculate_mean_returns",
+    "TransformError",
+]
